b55d1d4c835c40b2a5c016fa070919ed82a2869e
17863026867@163.com <17863026867@163.com>
2022-05-05 22:23:47 +0800

    用来测试持续构建

---
+++ Jenkins_build/PushCodeToLogin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author  : Bill
+# DateTime:
+
+
+import time
+
+print("------------")
+print("这是用来测试拉取git 代码自动化构建")
+print("hello" + " " + str(time.strftime("%Y-%m-%d %H:%M:%S")))
+print("ending------")
